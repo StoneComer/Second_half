d9e34161f832d495bc7c6f23458333d727cf6c42
StoneComer <fominm8@gmail.com>
2023-02-17 19:01:12 +0300

    rfJS

--- second_half/src/app/app.component.ts
+++ second_half/src/app/app.component.ts
@@ -1,14 +1,44 @@
-import { Component, Input } from '@angular/core';
-import { ResponseService } from './response.service';
+import { Component, VERSION } from '@angular/core';
+import { filter, interval, Subscription } from 'rxjs';
+import { pipe, map } from 'rxjs';
 
 @Component({
-  selector: 'app-root',
+  selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  constructor(
-    public RespServ: ResponseService
-  ){}
   title = 'second_half';
+  constructor() {}
+
+  intervalSubs$: Subscription = new Subscription;
+  intervalSubsRand$: Subscription = new Subscription;
+  enableStopButton = false;
+  array:number [] = [];
+  arrayRand:string [] = [];
+
+  getCounter() {
+    const intervalStream$ = interval(2000);
+    this.enableStopButton = true;
+    this.intervalSubs$ = intervalStream$.subscribe((value) => {
+      this.array.push(value);
+    });
+    this.intervalSubsRand$ = intervalStream$.pipe(
+      map(value  => `Random Value: ${Math.floor(getRandomArbitrary(0,1000))}`))
+    .subscribe((value) => {
+      this.arrayRand.push(value);
+    });
+  }
+
+  stopSubs() {
+    this.intervalSubs$.unsubscribe();
+  }
+  
+  stopSubsRand() {
+    this.intervalSubsRand$.unsubscribe();
+  }
+}
+
+function getRandomArbitrary(min: number, max: number) {
+  return Math.random() * (max - min) + min;
 }
